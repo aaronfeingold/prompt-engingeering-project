07089cda1e4b4720f79fd5efd8ded896875d220c
aaronfeingold <ajfeingold88@gmail.com>
2024-07-17 11:42:16 -0400

    open ai service fn gereate resposne extended to take model, role and max tokens as params. next we will use a lib to count tokens to limit requests that would cause excessive API consumption

--- app/services/openai_service.py
+++ app/services/openai_service.py
@@ -5,8 +5,11 @@ class OpenAIService:
     def __init__(self, api_key):
         self.client = OpenAI(api_key=api_key)
 
-    def generate_response(self, prompt):
+    def generate_response(
+        self, prompt, model="gpt-3.5-turbo", role="user", max_tokens=500
+    ):
         return self.client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[{"role": "user", "content": prompt}],
+            model=model,
+            messages=[{"role": role, "content": prompt}],
+            max_tokens=max_tokens,
         )

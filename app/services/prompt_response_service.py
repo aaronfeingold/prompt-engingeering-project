7722756d859b73074346f062d65b71731d8d6785
aaronfeingold <ajfeingold88@gmail.com>
2024-07-22 16:22:06 -0400

    some error handling on the query all method

--- app/services/prompt_response_service.py
+++ app/services/prompt_response_service.py
@@ -1,25 +1,28 @@
+from flask import current_app
 import time
 from app.models import PromptResponse
 
 
 class PromptResponseService:
     @staticmethod
-    def create_new_prompt_response(openai_service, prompt):
+    def create_new_prompt_response(messages):
         start_time = time.time()
-        response = openai_service.generate_response(prompt)
+        response = current_app.openai_service.generate_response(messages)
         response_time = time.time() - start_time
         content, role = response.choices[0].message.content, response.choices[0].message.role
 
         prompt_response = PromptResponse(
-            prompt=prompt,
-            content=content,
-            role=role,
-            response_time=response_time
+            prompt=messages, content=content, role=role, response_time=response_time
         )
         prompt_response.add_to_db()
         return prompt_response.to_dict()
 
     @staticmethod
     def get_all_prompt_responses():
-        prompt_responses = PromptResponse.query.all()
-        return [response.to_dict() for response in prompt_responses]
+        try:
+            prompt_responses = PromptResponse.query.all()
+            return [response.to_dict() for response in prompt_responses]
+        except Exception as e:
+            raise RuntimeError(
+                "Failed to fetch prompt responses from the database."
+            ) from e

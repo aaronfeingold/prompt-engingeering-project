5fc96d2842188508308ace4aaa7dc6168a2d90f4
aaronfeingold <ajfeingold88@gmail.com>
2024-07-24 16:37:55 -0400

    modify the run.py file to make the entry point for poetry a little more clear

--- run.py
+++ run.py
@@ -3,5 +3,10 @@ from app import create_app
 # initialize the app
 app = create_app()
 
-if __name__ == "__main__":
+
+def main():
     app.run(debug=True)
+
+
+if __name__ == "__main__":
+    main()

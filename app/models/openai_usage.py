f96a05d0f2d6110c97aa44fda69589ea48eb1b55
aaronfeingold <ajfeingold88@gmail.com>
2024-07-24 17:40:18 -0400

    new db table for opanai usage

---
+++ app/models/openai_usage.py
@@ -0,0 +1,22 @@
+from app.database import db
+from datetime import datetime
+
+
+class OpenAIUsage(db.Model):
+    __tablename__ = "usage_data"
+    id = db.Column(db.Integer, primary_key=True)
+    prompt_response_id = db.Column(
+        db.Integer, db.ForeignKey("prompt_responses.id"), nullable=False
+    )
+    completion_tokens = db.Column(db.Integer, nullable=False)
+    prompt_tokens = db.Column(db.Integer, nullable=False)
+    total_tokens = db.Column(db.Integer, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def add_to_db(self):
+        db.session.add(self)
+        try:
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            raise RuntimeError(f"Insertion to the OpenAIUsage table failed: {e}")

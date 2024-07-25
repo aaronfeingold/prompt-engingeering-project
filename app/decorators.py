aad38d2d8aa7f687ac44bf89f47c5eb8f1c49731
aaronfeingold <ajfeingold88@gmail.com>
2024-07-25 16:01:27 -0400

    new decorator to check if the user is allowed to use this route.

---
+++ app/decorators.py
@@ -0,0 +1,22 @@
+from functools import wraps
+from flask_jwt_extended import get_jwt_identity
+from flask import jsonify
+from app.models import User
+
+
+def role_required(required_roles):
+    def decorator(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            user_id = get_jwt_identity()
+            user = User.query.get(user_id)
+            if not any(role in user.roles for role in required_roles):
+                return (
+                    jsonify({"message": "Access forbidden: insufficient permissions"}),
+                    403,
+                )
+            return f(*args, **kwargs)
+
+        return decorated_function
+
+    return decorator

d632b120b3836872c915862932cf66a1028b4592
aaronfeingold <ajfeingold88@gmail.com>
2024-07-25 16:04:50 -0400

    jwt required for these routes

--- app/routes/openai.py
+++ app/routes/openai.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, request
+from flask_jwt_extended import jwt_required
 from app.controllers import create_new_prompt_response, query_prompt_responses
 from app.decorators import role_required
 
@@ -6,12 +7,14 @@ bp = Blueprint("openai", __name__)
 
 
 @bp.route("/prompt", methods=["POST"])
+@jwt_required()
 @role_required(["admin", "user"])
 def post_prompt():
     return create_new_prompt_response(request)
 
 
 @bp.route("/prompt-responses", methods=["GET"])
+@jwt_required()
 @role_required(["admin", "user"])
 def get_prompt_responses():
     return query_prompt_responses(request)

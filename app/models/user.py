3df62462cf736581bdbb347a79569eed70da083e
aaronfeingold <ajfeingold88@gmail.com>
2024-07-25 12:41:05 -0400

    add in db from app, and a new instace of the Bcrypt libary

--- app/models/user.py
+++ app/models/user.py
@@ -2,3 +2,6 @@ from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_bcrypt import Bcrypt
 from datetime import datetime
+from app.database import db
+
+bcrypt = Bcrypt()

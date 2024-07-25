44043d62ca7dd64310e2d0bafc8486692960d66a
aaronfeingold <ajfeingold88@gmail.com>
2024-07-25 12:47:50 -0400

    user model is mostly squared away for now

--- app/models/user.py
+++ app/models/user.py
@@ -1,7 +1,30 @@
-from flask_sqlalchemy import SQLAlchemy
-from werkzeug.security import generate_password_hash, check_password_hash
 from flask_bcrypt import Bcrypt
 from datetime import datetime
 from app.database import db
 
 bcrypt = Bcrypt()
+
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    two_fa_setup = db.Column(db.Boolean, default=False)
+    roles = db.Column(db.ARRAY(db.Integer), nullable=False)
+    teams = db.Column(db.ARRAY(db.Integer), nullable=True)
+    regular_budget = db.Column(db.Integer, default=0)
+    team_budgeted = db.Column(db.Boolean, default=False)
+    temporary_budget = db.Column(db.Integer, default=0)
+    temporary_budget_expiration = db.Column(db.DateTime, nullable=True)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    last_updated = db.Column(
+        db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow
+    )
+
+    def set_password(self, password):
+        self.password_hash = bcrypt.generate_password_hash(password).decode("utf-8")
+
+    def check_password(self, password):
+        return bcrypt.check_password_hash(self.password_hash, password)

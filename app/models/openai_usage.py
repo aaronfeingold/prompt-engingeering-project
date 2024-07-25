5da43d2e3862a6bf58514e8347ba19a6383e1b1a
aaronfeingold <ajfeingold88@gmail.com>
2024-07-25 15:35:20 -0400

    OpenAIUsage table now has assiciation via foreign key to users and teams table. plus, there is as a back popular on user, team and prompt_response back population

--- app/models/openai_usage.py
+++ app/models/openai_usage.py
@@ -8,10 +8,15 @@ class OpenAIUsage(db.Model):
     prompt_response_id = db.Column(
         db.Integer, db.ForeignKey("prompt_response.id"), nullable=False
     )
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    team_id = db.Column(db.Integer, db.ForeignKey("teams.id"), nullable=True)
     completion_tokens = db.Column(db.Integer, nullable=False)
     prompt_tokens = db.Column(db.Integer, nullable=False)
     total_tokens = db.Column(db.Integer, nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    user = db.relationship("User", back_populates="usages")
+    team = db.relationship("Team", back_populates="usages")
+    prompt_response = db.relationship("PromptResponse", back_populates="usages")
 
     def add_to_db(self):
         db.session.add(self)

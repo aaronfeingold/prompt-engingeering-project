d28122499b2cf4438fe235d152b064d36fdb8914
aaronfeingold <ajfeingold88@gmail.com>
2024-07-25 16:03:51 -0400

    use the role_required to check that this is a user or admin first, and this will also check the user identiy pulled from the JWT token.

--- app/routes/openai.py
+++ app/routes/openai.py
@@ -1,15 +1,17 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request
 from app.controllers import create_new_prompt_response, query_prompt_responses
-
+from app.decorators import role_required
 
 bp = Blueprint("openai", __name__)
 
 
 @bp.route("/prompt", methods=["POST"])
+@role_required(["admin", "user"])
 def post_prompt():
     return create_new_prompt_response(request)
 
 
 @bp.route("/prompt-responses", methods=["GET"])
+@role_required(["admin", "user"])
 def get_prompt_responses():
     return query_prompt_responses(request)

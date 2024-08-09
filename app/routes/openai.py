ddff232c10eb820bfa71c631b9de0f999a0510b5
aaronfeingold <ajfeingold88@gmail.com>
2024-08-09 14:09:40 -0500

    use flask jwt extended to get the jwt identity, for post prompt and get. also, a list of users can be passed, but only if admin or a team leader and these are teamates then we will query for the data

--- app/routes/openai.py
+++ app/routes/openai.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request
-from flask_jwt_extended import jwt_required
+from flask_jwt_extended import jwt_required, get_jwt_identity
 from app.controllers import create_new_prompt_response, query_prompt_responses
 from app.decorators import role_required
 
@@ -10,11 +10,14 @@ openai_bp = Blueprint("openai", __name__)
 @jwt_required()
 @role_required(["admin", "user"])
 def post_prompt():
-    return create_new_prompt_response(request)
+    user_identity = get_jwt_identity()
+    return create_new_prompt_response(request, user_identity)
 
 
 @openai_bp.route("/prompt-responses", methods=["GET"])
 @jwt_required()
 @role_required(["admin", "user"])
 def get_prompt_responses():
-    return query_prompt_responses(request)
+    user_identity = get_jwt_identity()
+    user_list = request.args.getlist("users")
+    return query_prompt_responses(request, user_identity, user_list)

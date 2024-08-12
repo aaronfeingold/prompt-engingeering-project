901e36021122f7fee0791af9a20f9cec72120c38
aaronfeingold <ajfeingold88@gmail.com>
2024-08-12 16:08:52 -0500

    add team info to the prompt model

--- app/models/prompt_response.py
+++ app/models/prompt_response.py
@@ -14,6 +14,7 @@ class PromptResponse(db.Model):
     prompts = db.Column(JSONB, nullable=False)
     usages = db.relationship("OpenAIUsage", back_populates="prompt_response")
     user = db.relationship("User", back_populates="prompt_responses")
+    team = db.relationship("Team", back_populates="prompt_responses")
     response_time = db.Column(db.Float, nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
 

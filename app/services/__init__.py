c9a8a237a251df87d4d72ed4bab882b817813756
aaronfeingold <ajfeingold88@gmail.com>
2024-08-12 13:03:40 -0500

    test if unused import in init files it ok

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,3 +1,3 @@
 from .prompt_response_service import PromptResponseService  # noqa: F401
 from .openai_service import OpenAIService  # noqa: F401
-from .user_service import UserService  # noqa: F401
+from .user_service import UserService

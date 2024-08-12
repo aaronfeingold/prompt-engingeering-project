8a90f2f7dba4e77a7c71ad469e7289c1b4f317a3
aaronfeingold <ajfeingold88@gmail.com>
2024-08-12 14:21:59 -0500

    admin and user can now send a request for a new prompt

--- app/routes/openai.py
+++ app/routes/openai.py
@@ -2,13 +2,14 @@ from flask import Blueprint, request
 from flask_jwt_extended import jwt_required
 from app.controllers import create_new_prompt_response, query_prompt_responses
 from app.decorators import role_required
+from app.models.user import RoleEnum
 
 openai_bp = Blueprint("openai", __name__)
 
 
 @openai_bp.route("/prompt", methods=["POST"])
 @jwt_required()
-@role_required(["admin", "user"])
+@role_required([RoleEnum.USER.name, RoleEnum.ADMIN.name])
 def post_prompt():
     return create_new_prompt_response(request)
 

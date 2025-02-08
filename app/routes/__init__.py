32443391f561cd1d223bdb7ee50530a0538fe63a
aaron.feingold <ajfeingold88@gmail.com>
2025-02-07 23:50:45 -0600

    use the team blueprint

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from app.routes.llm import llm_bp
 from app.routes.user import user_bp
 from app.routes.usage import usage_bp
+from app.routes.team import team_bp
 
 api_version = "v1"
 
@@ -11,3 +12,4 @@ def register_routes(app: Flask):
     app.register_blueprint(llm_bp, url_prefix=f"{api_prefix}/llm")
     app.register_blueprint(user_bp, url_prefix=f"{api_prefix}/user")
     app.register_blueprint(usage_bp, url_prefix=f"{api_prefix}/usage")
+    app.register_blueprint(team_bp, url_prefix=f"{api_prefix}/team")

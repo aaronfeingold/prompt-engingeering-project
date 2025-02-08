0a138b411aae7bcac9d2967c4b4cf376b7f718cc
aaron.feingold <ajfeingold88@gmail.com>
2025-02-07 20:31:30 -0600

    teams service class exported

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -2,3 +2,4 @@ from .prompt_response_service import PromptResponseService
 from .openai_service import OpenAIService
 from .user_service import UserService
 from .token_service import estimate_tokens
+from .team_service import TeamService
